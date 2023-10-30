92945dd1b5fa2f6980b4dcfa4e4129db7116a00b
Chiriac D. Sebastian-Ioan <cti22a116@cst.uoradea.local>
2023-10-30 19:04:58 +0200

    Initial commit

---
+++ src/ro/uoradea/Dog.java
@@ -0,0 +1,11 @@
+package ro.uoradea;
+
+public class Dog {
+    String name;
+    int age'
+        '
+}
+public Dog(String num, int i){
+    name = num;
+    age = i;
+}
---
+++ src/ro/uoradea/Main.java
@@ -0,0 +1,10 @@
+package ro.uoradea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Dog dog1 = new Dog(num= "Rufus", i= 3);
+    Dog dog1 = new Dog(num= "Rex", i= 5);
+    System.out.println("Name: "+ dog1.name + "age: " + dog1.age);
+    }
+}
